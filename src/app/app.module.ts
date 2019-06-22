4850519a93b0912a96df32ff63967c4cddc2aa6d
jcsouza <jcsouza@latam.stefanini.com>
2019-06-22 18:57:14 -0300

    Curso angular devmedia - Aula 11  - Adicionando o Menu Lateral

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PainelSimplesComponent } from './painel-simples/painel-simples.component';
 import { BarraNavegacaoComponent } from './barra-navegacao/barra-navegacao.component';
+import { MenuLateralComponent } from './barra-navegacao/menu-lateral/menu-lateral.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PainelSimplesComponent,
     BarraNavegacaoComponent,
+    MenuLateralComponent,
   ],
   imports: [
     BrowserModule
