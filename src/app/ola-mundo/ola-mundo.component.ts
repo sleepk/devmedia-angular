32913bb56ffe585720d950e674452efd81a3603d
jcsouza <jcsouza@latam.stefanini.com>
2019-06-22 13:59:16 -0300

    Curso angular devmedia - Aula 4  - Criando um componente Olá Mundo

---
+++ src/app/ola-mundo/ola-mundo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+//informacoes de como sera passado as informacoes para o html
+//onde sera passado os seletores, template html e o css
+@Component({
+selector:'ola-mundo',
+templateUrl:'./ola-mundo.component.html',
+styleUrls:['./ola-mundo.component.css'],
+
+})
+export class OlaMundoComponent{
+
+}
