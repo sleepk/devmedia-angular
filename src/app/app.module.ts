cecd76d0f5eb1334d71c9b2dc412a32ed1ba1a33
jcsouza <jcsouza@latam.stefanini.com>
2019-06-22 16:36:15 -0300

    Curso Angular Devmedia - Aula 10 - Adicionando a Barra de Navegação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PainelSimplesComponent } from './painel-simples/painel-simples.component';
+import { BarraNavegacaoComponent } from './barra-navegacao/barra-navegacao.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PainelSimplesComponent,
+    BarraNavegacaoComponent,
   ],
   imports: [
     BrowserModule
